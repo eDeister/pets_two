6668aa16d04b185f4ef9f6b4aa94b0985f302ecb
edeister <deister.ethan@student.greenriver.edu>
2024-04-19 19:29:30 -0400

    Added input validation, reroute to order summary, and summary page with templating.

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
-//Set error reporting to true
+//Set error reporting to true, require autoload, and start session
 ini_set('error_reporting',1);
 error_reporting(E_ALL);
-
-//Require autoload.php
+session_start();
 require_once('vendor/autoload.php');
 
 //Instantiate f3 base class
@@ -17,9 +16,35 @@ $f3->route('GET /', function() {
 });
 
 //Route to order form
-$f3->route('GET|POST /order', function() {
+$f3->route('GET|POST /order', function($f3) {
+
+    //Check if the form was posted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //Get data
+        $color = $_POST['color'];
+        $pet = $_POST['type'];
+
+        //Validate the data
+        if (!isset($_POST['color']) || !isset($_POST['type'])) {
+            echo 'Please enter a pet type.';
+            echo var_dump($_POST);
+        } else {
+            //Data is valid
+            $f3->set('SESSION.color', $color);
+            $f3->set('SESSION.type', $pet);
+            $f3->reroute('summary');
+        }
+    } else {
+        echo 'get method';
+    }
+
     $view = new Template();
     echo $view->render('views/pet-order.html');
+});
+
+$f3->route('GET /summary', function() {
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
 });
 
 //Run fat free
